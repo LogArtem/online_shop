5e102a6fac591ffcc1105e77638c51a10a4e9dd2
Artem <alogvinenko1001@gmail.com>
2026-01-31 15:23:42 +0200

    lesson 5

--- app/routes.py
+++ app/routes.py
@@ -23,3 +23,38 @@ def add_product():
         flash('Product added!')
         return redirect(url_for('routes.products'))
     return render_template('product_form.html', action='Add', product=None)
+
+
+@bp.route('/delete/<int:product_id>', methods=['POST'])
+def delete_product(product_id):
+    product = Product.query.get_or_404(product_id)
+    db.session.delete(product)
+    db.session.commit()
+    flash('Product deleted!')
+    return redirect(url_for('routes.products'))
+
+
+@bp.route('/update/<int:product_id>', methods=['GET', 'POST'])
+def update_product(product_id):
+    product = Product.query.get_or_404(product_id)
+
+    if request.method == 'POST':
+        product.name = request.form['name']
+        product.price = float(request.form['price'])
+        product.description = request.form['description']
+        product.stock = int(request.form['stock'])
+        product.is_active = bool(request.form.get('is_active'))
+        product.category = request.form['category']
+        product.rating = float(request.form['rating'])
+        product.sale = float(request.form['sale'])
+
+        print(f'''name={product.name}, price={product.price}, 
+description={product.description}, stock={product.stock}, 
+is_active={product.is_active}, category={product.category}, 
+rating={product.rating}, sale={product.sale}''')
+
+        db.session.commit()
+        flash('Product updated!')
+        return redirect(url_for('routes.product'))
+
+    return render_template('product_form.html', action='Update', product=product)
